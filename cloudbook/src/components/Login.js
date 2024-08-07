e3f666c2367413141d2505f7e63824b8c20ac15b
asadsk8r02 <asadkhanworkmail@gmail.com>
2024-08-06 23:11:43 -0500

    3

--- cloudbook/src/components/Login.js
+++ cloudbook/src/components/Login.js
@@ -36,7 +36,8 @@ const Login = (props) => {
   };
 
   return (
-    <div>
+    <div className="mt-3">
+      <h2>Login to continue to Cloudbook</h2>
       <form onSubmit={handleSubmit}>
         <div className="mb-3">
           <label htmlFor="email" className="form-label">
